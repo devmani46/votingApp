c861449a3b21d1758ecb40887ac0214bd47a88bb
devmani <devmani1456@gmail.com>
2025-09-02 14:30:06 +0545

    making campaign card component

--- src/app/pages/campaign-status/campaign-status.ts
+++ src/app/pages/campaign-status/campaign-status.ts
@@ -1,11 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CampaignCard } from '../../components/campaign-card/campaign-card';
 
 @Component({
   selector: 'app-campaign-status',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, CampaignCard],
   templateUrl: './campaign-status.html',
   styleUrl: './campaign-status.scss'
 })
-export class CampaignStatus {
-
-}
+export class CampaignStatus {}
