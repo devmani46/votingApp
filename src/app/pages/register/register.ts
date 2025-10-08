97d619d55aead934b2781a135f1a852553157502
devmani <devmani1456@gmail.com>
2025-10-08 11:24:10 +0545

    some code fixes in register

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import {
   Validators,
   ReactiveFormsModule,
@@ -32,9 +32,7 @@ function matchFieldsValidator(
     const fg = group as FormGroup<RegisterForm>;
     const control = fg.controls[field];
     const matchControl = fg.controls[matchingField];
-
     if (!control || !matchControl) return null;
-
     if (!matchControl.value) {
       if (matchControl.errors?.['mismatch']) {
         const { mismatch, ...rest } = matchControl.errors;
@@ -42,7 +40,6 @@ function matchFieldsValidator(
       }
       return null;
     }
-
     if (control.value !== matchControl.value) {
       matchControl.setErrors({
         ...(matchControl.errors || {}),
@@ -66,14 +63,9 @@ function matchFieldsValidator(
   templateUrl: './register.html',
   styleUrls: ['./register.scss'],
 })
-export class Register implements OnInit {
-  form!: FormGroup<RegisterForm>;
-  submitted = false;
-
-  constructor(private fb: NonNullableFormBuilder, private auth: AuthService) {}
-
-  ngOnInit(): void {
-    this.form = this.fb.group<RegisterForm>(
+export class Register {
+  form = signal<FormGroup<RegisterForm>>(
+    this.fb.group<RegisterForm>(
       {
         firstName: this.fb.control('', Validators.required),
         lastName: this.fb.control('', Validators.required),
@@ -85,43 +77,64 @@ export class Register implements OnInit {
       {
         validators: [matchFieldsValidator('password', 'confirmPassword')],
       }
-    );
-  }
+    )
+  );
+
+  submitted = signal<boolean>(false);
+
+  firstName = computed(() => this.form().controls.firstName);
+  lastName = computed(() => this.form().controls.lastName);
+  username = computed(() => this.form().controls.username);
+  email = computed(() => this.form().controls.email);
+  password = computed(() => this.form().controls.password);
+  confirmPassword = computed(() => this.form().controls.confirmPassword);
+
+  firstNameError = computed(() =>
+    this.submitted() && this.firstName().hasError('required') ? 'First name is required' : ''
+  );
+
+  lastNameError = computed(() =>
+    this.submitted() && this.lastName().hasError('required') ? 'Last name is required' : ''
+  );
+
+  usernameError = computed(() =>
+    this.submitted() && this.username().hasError('required') ? 'Username is required' : ''
+  );
+
+  emailError = computed(() => {
+    if (!this.submitted()) return '';
+    if (this.email().hasError('required')) return 'Email is required';
+    if (this.email().hasError('email')) return 'Invalid email';
+    return '';
+  });
+
+  passwordError = computed(() =>
+    this.submitted() && this.password().hasError('required') ? 'Password is required' : ''
+  );
+
+  confirmPasswordError = computed(() => {
+    if (!this.submitted()) return '';
+    if (this.confirmPassword().hasError('required')) return 'Confirm password is required';
+    if (this.confirmPassword().hasError('mismatch')) return 'Passwords do not match';
+    return '';
+  });
+
+  constructor(private fb: NonNullableFormBuilder, private auth: AuthService) {}
 
   onSubmit() {
-    this.submitted = true;
-
-    if (this.form.valid) {
-      this.auth.register(this.form.getRawValue()).subscribe({
+    this.submitted.set(true);
+    if (this.form().valid) {
+      this.auth.register(this.form().getRawValue()).subscribe({
         next: () => {
           console.log('Registration successful');
         },
         error: (err) => {
           console.error('Registration failed:', err);
           alert('Registration failed. User may already exist with this email or username.');
-        }
+        },
       });
     } else {
-      this.form.markAllAsTouched();
+      this.form().markAllAsTouched();
     }
-  }
-
-  get firstName() {
-    return this.form.controls.firstName;
-  }
-  get lastName() {
-    return this.form.controls.lastName;
-  }
-  get username() {
-    return this.form.controls.username;
-  }
-  get email() {
-    return this.form.controls.email;
-  }
-  get password() {
-    return this.form.controls.password;
-  }
-  get confirmPassword() {
-    return this.form.controls.confirmPassword;
   }
 }
